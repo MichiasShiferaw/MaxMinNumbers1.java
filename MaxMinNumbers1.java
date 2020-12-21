19a844a178a79f079f338cc50cf0d999939ebc43
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2020-12-20 21:24:38 -0500

    Update MaxMinNumbers1.java

--- MaxMinNumbers1.java
+++ MaxMinNumbers1.java
@@ -5,3 +5,19 @@ import java.util.Scanner;
 
 
 public class MaxMinNumbers1 {
+ 
+  public static void main(String[] arg) {
+    
+    // Creates an array with 10 indexes
+    int[] rangeArray = new int[10];
+    Random rangeVal = new Random();
+    //int minValue;
+    //int maxValue = 99;
+    
+    // Adds a random number from 1-99 into the array for every index
+    for (int index = 0; index < rangeArray.length; index++) {
+      rangeArray[index] = rangeVal.nextInt(40) + 60;
+    }
+
+    // Sort the array
+    Arrays.sort(rangeArray);
